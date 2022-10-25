fc24c006de013c6de20b0fc467f6df9ee9db1976
poopysmack <54606838+poopysmack@users.noreply.github.com>
2022-10-25 13:23:20 -0600

    start the API into the main page, cannot get it to show anything besides undefined.  Random generation of quote is working in console.log

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 "use strict";
 //alert("connected");
+document.querySelector("#btn-quote").addEventListener('click', getQuote);
+const randomQuote = document.querySelector("#random-quote")
+
+
+function getQuote() {
+    let num = Math.random() * 7268;
+    let page = Math.ceil(num);
+    let url = "https://quote-garden.herokuapp.com/api/v3/quotes?page=";
+
+    url += page;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let numQuote = Math.floor(Math.random() * 10);
+        console.log(numQuote);
+        console.log(data)
+        console.log(data.data[numQuote]);
+        randomQuote.innerHTML = `<h1>${data.data.quoteText}</h1>`;
+        randomQuote.innerHTML += `<h4>${data.data.quoteAuthor}</h4>`;
+    })
+}
+
+
